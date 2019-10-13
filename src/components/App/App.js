0cc6856de06e254007fe1dfecd9885a0f231fea9
Jiwon Kang <jiwonk42@gmail.com>
2019-10-13 14:17:16 -0700

    moved state and API to the Series component from App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,16 +6,6 @@ import 'whatwg-fetch';
 
 
 class App extends Component {
-  state = {
-    series: []
-  };
-  
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-    .then((response) => response.json())
-    .then(json => this.setState({ series: json }))
-  }
-
   render() {
     return (
       <div className="App">
@@ -23,7 +13,6 @@ class App extends Component {
           <h1 className="App-title">TV Series App</h1>
         </header>
         <Intro message="Here you can find all of your loved series" />
-        The length of series array: {this.state.series.length}
         <Series />
       </div>
     );
--- src/containers/Series/Series.js
+++ src/containers/Series/Series.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 export class Series extends Component {
+  state = {
+    series: []
+  };
+  
+  componentDidMount() {
+    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+    .then((response) => response.json())
+    .then(json => this.setState({ series: json }))
+  }
+
   render() {
     return (
       <div>
-        Series
+        The length of series array: {this.state.series.length}
       </div>
     )
   }
