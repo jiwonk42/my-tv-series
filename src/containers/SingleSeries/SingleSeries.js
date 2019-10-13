68d44320cde57205065704036092057f1b223dbc
Jiwon Kang <jiwonk42@gmail.com>
2019-10-13 16:43:01 -0700

    create SingleSeries component and embed in the Main component with Route

---
+++ src/containers/SingleSeries/SingleSeries.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class SingleSeries extends Component {
+  render() {
+    return (
+      <div>
+        <p>Single Series</p>
+      </div>
+    )
+  }
+}
+
+export default SingleSeries;
